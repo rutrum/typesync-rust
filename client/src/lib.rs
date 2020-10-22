413a1d29818028886637d0767d6598316c67bf77
David Purdum <purdum41@gmail.com>
2020-10-22 19:22:31 -0400

    Add boilerplate seed code, add useful justfile commands

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,7 +1,45 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use seed::{prelude::*, *};
+
+enum Page {
+    Home,
+}
+
+struct Model {
+    page: Page,
+    color: String,
+}
+
+enum Msg {
+    Click,
+}
+
+fn update(msg: Msg, model: &mut Model, _orders: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::Click => {
+            model.color = if model.color == "red" {
+                "blue"
+            } else {
+                "red"
+            }.to_string()
+        },
     }
+}
+
+fn view(model: &Model) -> impl IntoNodes<Msg> {
+    div![
+        format!("{}", model.color), 
+        ev(Ev::Click, |_| Msg::Click), 
+    ]
+}
+
+fn init(_url: Url, _orders: &mut impl Orders<Msg>) -> Model {
+    Model {
+        page: Page::Home,
+        color: "red".to_string(),
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn render() {
+    App::start("app", init, update, view);
 }
--- justfile
+++ justfile
@@ -1,2 +1,11 @@
 test:
-    cargo test
+    cargo test --all
+
+build:
+    cargo build --all
+
+web:
+    (cd client; wasm-pack build --target web --out-name package --dev)
+
+serve: web
+    (cd client; microserver)
